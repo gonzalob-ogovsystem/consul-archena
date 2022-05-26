92869f30d9067b9a92120b5f719959c921355dee
Yamilet <yamil@yamil.org>
2022-05-26 17:26:19 +0200

    init test

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,11 @@
 module Consul
   class Application < Rails::Application
+    config.i18n.default_locale = :en
+    available_locales = [
+      "es",
+      "en",
+      ]
+    config.i18n.available_locales = available_locales
+    
   end
 end

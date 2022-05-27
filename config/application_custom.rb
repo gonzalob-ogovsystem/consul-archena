4b4c5edeff10faed41e7edbd738f58ea41e5ee78
Yamilet <yamil@yamil.org>
2022-05-27 10:27:50 +0200

    dgfd

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,11 +1,6 @@
 module Consul
   class Application < Rails::Application
-    config.i18n.default_locale = :en
-    available_locales = [
-      "es",
-      "en",
-      ]
-    config.i18n.available_locales = available_locales
-    
+    config.i18n.default_locale = :es
+    config.i18n.available_locales = [:es, :en]
   end
 end
